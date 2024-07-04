728e36b09072ead1a4f6ccfe897aa42cb8f96794
Ing-yeo_Developer <52366178+SeoJaeWan@users.noreply.github.com>
2024-07-04 10:52:00 +0900

    Update index.jsx

--- src/main/index.jsx
+++ src/main/index.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
-import Children1 from "../components/children1";
-import Children2 from "../components/children2";
-import Children3 from "../components/children3";
-import Children4 from "../components/children4";
+//import Children1 from "../components/children1";
+//import Children2 from "../components/children2";
+//import Children3 from "../components/children3";
+//import Children4 from "../components/children4";
 
-// const Children1 = React.lazy(() => import("../components/children1"));
-// const Children2 = React.lazy(() => import("../components/children2"));
-// const Children3 = React.lazy(() => import("../components/children3"));
-// const Children4 = React.lazy(() => import("../components/children4"));
+const Children1 = React.lazy(() => import("../components/children1"));
+const Children2 = React.lazy(() => import("../components/children2"));
+const Children3 = React.lazy(() => import("../components/children3"));
+const Children4 = React.lazy(() => import("../components/children4"));
 
 export default function Main() {
   return (
